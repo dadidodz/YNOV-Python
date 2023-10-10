a87e4cf172fc70ab8135a4fbd2c581d35ce15bad
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-10 17:16:40 +0200

    custom_calendar

---
+++ custom_calendar.py
@@ -0,0 +1,26 @@
+semaine = {
+    1 : 'Monday',
+    2 : 'Tuesday',
+    3 : 'Wednesday',
+    4 : 'Thursday',
+    5 : 'Friday',
+    6 : 'Saturday',
+    7 : 'Sunday',
+}
+
+def day_from_number(day_number):
+    listKeys = semaine.keys()
+    if 0<day_number<=len(listKeys):
+        return semaine[day_number]
+    else:
+        return None
+
+def day_to_number(day):
+    key_list = list(semaine.keys())
+    val_list = list(semaine.values())
+    if day not in val_list:
+        return None
+    else:
+        position = val_list.index(day)
+        if 0<position<8:
+            return (key_list[position])
--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-import shopping
+import custom_calendar
 
 if __name__ == '__main__':
-    print(shopping.clean_list(['tomatoes', 'pastas', 'milk', '  salt']))
-    print(shopping.clean_list(['tomatoes', 'pastas', 'salt']))
+    print(custom_calendar.day_from_number(2))
+    print(custom_calendar.day_from_number(1))
+    print(custom_calendar.day_from_number(1000))
+    print(custom_calendar.day_to_number('Sunday'))
+    print(custom_calendar.day_to_number('invalid day'))
