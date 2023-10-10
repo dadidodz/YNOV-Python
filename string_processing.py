2cc07d65256d0afcb5d9207cf665effad5bb5144
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-10 20:20:15 +0200

    string_processing

--- string_processing.py
+++ string_processing.py
@@ -1,9 +1,10 @@
-bad_chars = [';', ':', '!', "*", "?", ",", "-", "_", "."]
+bad_chars = [';', ':', '!', "*", "?", ",", "_", "."]
 
 def tokenize(sentence):
     for j in bad_chars:
         sentence = sentence.replace(j, '')
     token = sentence.replace("'", " ")
+    token = token.replace("-", " ")
     token = token.split()
     for i in range(len(token)):
         token[i] = token[i].lower()   
