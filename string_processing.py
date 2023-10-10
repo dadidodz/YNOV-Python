286f737cf3f5d4a6e9b57827e4141e4f063c3439
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-10 20:16:18 +0200

    string_processing

---
+++ string_processing.py
@@ -0,0 +1,10 @@
+bad_chars = [';', ':', '!', "*", "?", ","]
+
+def tokenize(sentence):
+    for j in bad_chars:
+        sentence = sentence.replace(j, '')
+    token = sentence.replace("'", " ")
+    token = token.split()
+    for i in range(len(token)):
+        token[i] = token[i].lower()   
+    return token
--- test.py
+++ test.py
@@ -1,8 +1,5 @@
-import custom_calendar
+import string_processing
 
 if __name__ == '__main__':
-    print(custom_calendar.day_from_number(2))
-    print(custom_calendar.day_from_number(1))
-    print(custom_calendar.day_from_number(1000))
-    print(custom_calendar.day_to_number('Sunday'))
-    print(custom_calendar.day_to_number('invalid day'))
+    my_sentence = "Dites, on n'attend pas votre soeur ?"
+    print(string_processing.tokenize(my_sentence))
