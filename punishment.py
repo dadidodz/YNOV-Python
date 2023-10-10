2ad8e7577d42ec7bd0315084d1b110f2585b42f3
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-10 15:32:38 +0200

    punishment

--- punishment.py
+++ punishment.py
@@ -1,11 +1,11 @@
 def do_punishment(first_part, second_part, nb_lines):
     first_part = first_part.strip()
     second_part = second_part.strip()
-    phrase = first_part + " " + second_part
-    if phrase.endswith(".")==False:
-        phrase+="."
-    if phrase.endswith(".")==True:
-        phrase += " "
+    phrase = first_part + ' ' + second_part
+    if phrase.endswith('.')==False:
+        phrase+='.'
+    if phrase.endswith('.')==True:
+        phrase += ' '
     phrase *= nb_lines
     phrase.strip()
     return phrase
