c63cc6d4ad44aef97d0d9f6acea420dac10f5ae3
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-10 14:51:43 +0200

    numerical_operations

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-from hello_world import say_hello_world
+import numerical_operations
 
 if __name__ == '__main__':
-    print(say_hello_world())
+    print(numerical_operations.add(2, 2))
+    print(numerical_operations.subtract(10, 5))
+    print(numerical_operations.multiply(3, 4))
+    print(numerical_operations.power(3, 3))
+    print(numerical_operations.square(3))
