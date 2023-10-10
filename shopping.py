0043d0cdbfcd947e266f3e19a997a9e6db9f308d
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-10 15:59:01 +0200

    shopping

--- shopping.py
+++ shopping.py
@@ -1,4 +1,4 @@
 def remember_the_milk(shopping_list):
-    if 'milk' not in shopping_list:
+    if 'milk' not in shopping_list and shopping_list:
         shopping_list.append('milk')
     return shopping_list
--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import shopping
 if __name__ == '__main__':
     list_with_milk = ['tomatoes', 'pastas', 'milk', 'salt']
     list_without_milk = ['tomatoes', 'pastas', 'salt']
+    list_without = []
 
     print(shopping.remember_the_milk(list_with_milk))
     print(shopping.remember_the_milk(list_without_milk))
+    print(shopping.remember_the_milk(list_without))
