bc455a8b1b6fcba84df501a5ff76fcecce03a0fe
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-10 16:26:38 +0200

    shopping

--- shopping.py
+++ shopping.py
@@ -4,12 +4,15 @@ def remember_the_milk(shopping_list):
     return shopping_list
 
 def clean_list(shopping_list):
-    remember_the_milk(shopping_list)
     i = 0
     if not shopping_list:
         return shopping_list
     while i < len(shopping_list):
         shopping_list[i] = shopping_list[i].strip()
+        i+=1
+    remember_the_milk(shopping_list)
+    i=0
+    while i < len(shopping_list):
         shopping_list[i] = shopping_list[i].capitalize()
         shopping_list[i] = f'{i+1}/ ' + shopping_list[i]
         i+=1    
