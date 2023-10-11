79b83b6960253c419a94b959a24e90527599c610
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-11 17:29:02 +0200

    decorator

--- decorator.py
+++ decorator.py
@@ -1,8 +1,7 @@
 import datetime as dt
 
 def with_current_date(func):
-
-    def wrapper(args, **kwargs):
+    def wrapper(*args, **kwargs):
         current_date = dt.date.today()
-        return func(args, current_date=current_date, **kwargs)
+        return func(*args, current_date=current_date, **kwargs)
     return wrapper
