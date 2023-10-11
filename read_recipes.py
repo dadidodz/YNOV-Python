1293e72ddfa3bc1d7792bbea87bbddea02e3911a
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-11 15:00:26 +0200

    read_file

--- read_recipes.py
+++ read_recipes.py
@@ -1,6 +1,6 @@
 import json
 
 def get_recipes(file_name):
-    file = open("recipes_data.json", "r", encoding="utf-8")
+    file = open(file_name, "r", encoding="utf-8")
     data = json.load(file)
     return data
