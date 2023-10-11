a2990bb96439b58786b5e90c8fa64b97aa55b273
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-11 14:53:26 +0200

    read_file

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import recipes
+import read_recipes
 
 if __name__ == '__main__':
-    print(recipes.create_recipe('Pâtes au beurre', 30, ['pâtes', 'beurre']))
+    print(read_recipes.get_recipes('recipes_data.json'))
