c23516e63d2218b621ba353fd61df11de5228356
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-11 16:30:42 +0200

    filter_recipes

--- filter_recipes.py
+++ filter_recipes.py
@@ -1,4 +1,4 @@
 def filter_recipes(recipes: list[dict], max_persons: int) -> list[str]:
-    newlist = [recipe['title'] for recipe in recipes if recipe['persons']<=max_persons]
+    newlist = [recipe['title'] for recipe in recipes if recipe['persons']<max_persons]
     return newlist
     
