bce606ab9fd257401bda87b498cb6ad4829621c8
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-11 17:16:35 +0200

    recipes2

--- recipes.py
+++ recipes.py
@@ -13,17 +13,16 @@ def create_recipe(nameb, personsb, ingredientsb):
     return recipe
 
 def create_recipe_v2(title, persons, *ingredients, **tags):
-    if len(title) > 40:
-        raise ValueError("Title is too long")
-    if persons <= 0 or persons > 20:
-        raise ValueError("Invalid persons number")
-    if not ingredients:
-        raise ValueError("Ingredient list cannot be empty")
-
-    return {
+    recipe = {
         'title': title,
         'persons': persons,
         'ingredients': ingredients,
         'tags': tags
     }
-    
+    if len(recipe['title'])>150:
+        raise ValueError("Title is too long")
+    if recipe['persons']==False or recipe['persons']>50:
+        raise ValueError("Invalid persons number")
+    if not len(recipe['ingredients']):
+        raise ValueError("Ingredient list cannot be empty")
+    return recipe
