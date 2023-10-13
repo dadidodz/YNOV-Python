3368053bb546003dfbb783be4da5c91f28dbb83d
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-13 13:00:19 +0200

    spaceships2

--- test.py
+++ test.py
@@ -2,15 +2,10 @@ from spaceships import Interceptor, Bomber, Destroyer
 
 if __name__ == '__main__':
     tie_interceptor = Interceptor()
-    y_wing = Bomber()
-
-    print(tie_interceptor.attack)
+    y_wing = Destroyer()
+    print(y_wing.attack)
     print(y_wing.defense)
 
-    tie_interceptor.fire_on(y_wing)
-    print(y_wing.defense)
-
-    venator = Destroyer()
-    print(venator.defense)
-    tie_interceptor.fire_on(venator)
-    print(venator.defense)
+    print(tie_interceptor.defense)
+    y_wing.fire_on(tie_interceptor)
+    print(tie_interceptor.defense)
