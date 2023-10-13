a8b4623cf7b89c9e9c9e1ade74d5823c31a0638d
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-13 14:04:27 +0200

    spaceships2

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
-from spaceships import BattleshipKiller, Interceptor, Bomber, Destroyer
+from spaceships import BattleshipKiller, Frigate, Interceptor, Bomber, Destroyer
 
 if __name__ == '__main__':
     ship = BattleshipKiller()
     print(ship.attack)
+    print(ship.defense)
+    # fighter = Bomber()
+    # print(fighter.defense)
+    battleship = Frigate()
+    print(battleship.defense)
+    ship.fire_on(battleship)
+    print(battleship.defense)
