f38d987f77b8a7a5e1ad6241203d75117a467940
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-13 13:47:20 +0200

    spaceships2

--- spaceships.py
+++ spaceships.py
@@ -1,38 +1,42 @@
 from base_spaceships import Battleship, Fighter, Spaceship
 
-class FighterKiller():
+class FighterKiller(Spaceship):
+    def __init__(self, attack=800, defense=2800):
+        super().__init__(attack, defense)
+
     def fire_on(self, spaceship):
         if isinstance(spaceship, Fighter):
-            return spaceship.take_damages(self.attack*2)
+            spaceship.take_damages(self.attack*2)
         else:
-            return spaceship.take_damages(self.attack)
+            spaceship.take_damages(self.attack)
 
-class BattleshipKiller():
+class BattleshipKiller(Spaceship):
+    def __init__(self, attack=800, defense=900):
+        super().__init__(attack, defense)
+    
     def fire_on(self, spaceship):
         if isinstance(spaceship, Battleship): 
-            return spaceship.take_damages(self.attack*2)
+            spaceship.take_damages(self.attack*2)
         else:
-            return spaceship.take_damages(self.attack)
-
-
+            spaceship.take_damages(self.attack)
 
 class Cruiser (Battleship):
     def __init__(self, attack=800, defense=3000):
         super().__init__(attack, defense)
 
-class Bomber(BattleshipKiller, Fighter):
+class Bomber(Fighter, BattleshipKiller):
     def __init__(self, attack=150, defense=2000):
         super().__init__(attack, defense)
 
-class Destroyer (BattleshipKiller, Battleship):
+class Destroyer (Battleship, BattleshipKiller):
     def __init__(self, attack=650, defense=5000):
         super().__init__(attack, defense)
 
-class Interceptor(FighterKiller, Fighter):
+class Interceptor(Fighter, FighterKiller):
     def __init__(self, attack=180, defense=1000):
         super().__init__(attack, defense)
 
-class Frigate (FighterKiller, Battleship):
+class Frigate (Battleship, FighterKiller):
     def __init__(self, attack=500, defense=2500):
         super().__init__(attack, defense)
 
--- test.py
+++ test.py
@@ -1,16 +1,5 @@
-from spaceships import Interceptor, Bomber, Destroyer
+from spaceships import BattleshipKiller, Interceptor, Bomber, Destroyer
 
 if __name__ == '__main__':
-    tie_interceptor = Interceptor()
-    y_wing = Bomber()
-
-    print(tie_interceptor.attack)
-    print(y_wing.defense)
-
-    tie_interceptor.fire_on(y_wing)
-    print(y_wing.defense)
-
-    venator = Destroyer()
-    print(venator.defense)
-    tie_interceptor.fire_on(venator)
-    print(venator.defense)
+    ship = BattleshipKiller()
+    print(ship.attack)
