759f4724e61e94356634ca857b31f395d2837a3b
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-11-11 18:26:49 +0100

    pickle

--- dock_repositories.py
+++ dock_repositories.py
@@ -1,10 +1,29 @@
 from dock import SpaceDock
+import pickle, os
 
 class SpaceDockFileRepository:
 
+    def __init__(self, name :str) -> None:
+        self.name = name
+
     def save(self, dock: SpaceDock) -> None:
-        pass
+        self.picklefile = open(self.name, "wb")
+
+        pickle.dump(dock, self.picklefile)
+
+        self.picklefile.close()
 
 
     def load(self) -> SpaceDock:
-        pass
+        if not os.path.exists(self.name):
+            spacedock = SpaceDockFileRepository(self.name)
+            spacedock.load(self.name)
+            return spacedock
+        else:
+            picklefile = open(self.name, "rb")
+
+            output = pickle.load(picklefile)
+
+            return output
+
+        
--- test.py
+++ test.py
@@ -1,19 +1,15 @@
 from dock import SpaceDock
+from dock_repositories import SpaceDockFileRepository
 from spaceships import Interceptor, Frigate
 
 if __name__ == '__main__':
     dock = SpaceDock()
-
-    default_fleet = dock['default']
-    print(default_fleet.ships)
-
-    new_fleet = dock['this fleet does not exist yet']
-    print(new_fleet.name, new_fleet.ships)
-
-    dock['my new fleet'] = [Interceptor(), Interceptor(), Frigate()]
-    print(dock['my new fleet'].name, len(dock['my new fleet'].ships))
-
+    dock['default'] = [Interceptor(), Interceptor()]
+    dock['defenders'] = [Frigate(), Frigate(), Interceptor()]
     print(dock)
     
-    del dock['this fleet does not exist yet']
-    print(dock)
+    dock_repo = SpaceDockFileRepository('fleets.pickle')
+    dock_repo.save(dock)
+    
+    retrieved_dock = dock_repo.load()
+    print(retrieved_dock)
