34e2e8b94eb81a85ab685ab3bdd33f375d6ade60
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-11-14 17:04:42 +0100

    match_cases

--- base_spaceships.py
+++ base_spaceships.py
@@ -1,22 +1,30 @@
+from requirements import Requirements
 class Spaceship:
     """
     Spaceship class
+
     Attribut : 
         is_alive(bool):Statut of the spaceship, dead of alive, default value is True.
     """
     is_alive = True
-    def __init__(self, attack=100, defense=100):
+    requirements : Requirements
+
+    def __init__(self, requirements, attack=100, defense=100):
         """
         Initialize the attributs attack and defense with the value of the parameters.
+
         Parameters:
         attack (int):The value of the attack of ship.
         defense (int):The value of the defense of ship.
         """
+        self.requirements = requirements
         self.attack = attack
         self.defense = defense
+        
     def take_damages(self, damage):
         """
         Subtract the damage passed in parameters to the ship defense.
+
         Parameters:
         damage (int):The value of the damage the ship takes. Can't be negative.
         """
@@ -30,10 +38,12 @@ class Spaceship:
     def fire_on(self, spaceship):
         """
         Deals damages to the spaceship passed in parameters.
+
         Parameters:
         spaceship (Spaceship):Targetted spaceship.
         """
         spaceship.take_damages(self.attack)
+
 class Battleship(Spaceship):
     """
     A class representing a battleship, inheriting from Spaceship.
