78f532fa8ee0693688f38e56adc98b2935bc698e
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-11-14 17:02:16 +0100

    random

--- battle_simulator.py
+++ battle_simulator.py
@@ -1,19 +1,11 @@
 import random
 from base_spaceships import Spaceship
-from fleet import Fleet
-
 class Simulator:
-
-    def __init__ (self, attacker_fleet :Fleet, defender_fleet: Fleet) -> None:
-        self.attacket_fleet = attacker_fleet
-        self.defender_fleet = defender_fleet
-
     @staticmethod
     def _duel_fight(attack_spaceship : Spaceship, defense_spaceship : Spaceship) -> None:
         attack_spaceship.fire_on(defense_spaceship)
-        if defense_spaceship.is_alive:
+        if defense_spaceship.is_alive == True:
             defense_spaceship.fire_on(attack_spaceship)
-
     def _simulate_fight(self, attacker_ships_list:list, defenser_ships_list:list):
         if not len(attacker_ships_list) or not len(defenser_ships_list):
             pass
@@ -21,14 +13,3 @@ class Simulator:
             for ship in attacker_ships_list:
                 if ship.is_alive:
                     self._duel_fight(ship, defenser_ships_list[random.randint(0, len(defenser_ships_list)-1)])
-
-    def fight(self):
-        self._simulate_fight(self.attacket_fleet.alive_battleships, self.defender_fleet.alive_battleships)
-        self._simulate_fight(self.attacket_fleet.alive_fighters, self.defender_fleet.alive_fighters)
-        self._simulate_fight(self.attacket_fleet.alive_ships, self.defender_fleet.alive_ships)
-
-    def get_report(self):
-        return {
-            self.attacket_fleet.name: self.attacket_fleet.report,
-            self.defender_fleet.name: self.defender_fleet.report,
-        }
--- spaceships.py
+++ spaceships.py
@@ -1,6 +1,4 @@
 from base_spaceships import Battleship, Fighter, Spaceship
-from requirements import Requirements
-
 class FighterKiller(Spaceship):
     def fire_on(self, target: Spaceship):
         if target.__class__.__base__.__name__ == "Fighter" or target.__class__.__name__ == "Fighter":
@@ -8,7 +6,6 @@ class FighterKiller(Spaceship):
         else:
             target.take_damages(self.attack)
         return target
-
 class BattleshipKiller(Spaceship):
     def fire_on(self, target: Spaceship):
         if target.__class__.__base__.__name__ == "Battleship" or target.__class__.__name__ == "Battleship":
@@ -16,28 +13,18 @@ class BattleshipKiller(Spaceship):
         else:
             target.take_damages(self.attack)
         return target
-
 class Cruiser (Battleship):
-    requirements = Requirements(25000, 10000)
     def __init__(self, attack=800, defense=3000) -> None:
         super().__init__(attack, defense)
-
 class Bomber(Fighter, BattleshipKiller):
-    requirements = Requirements(2500, 400)
     def __init__(self, attack=150, defense=2000) -> None:
         super().__init__(attack, defense)
-
 class Destroyer (Battleship, BattleshipKiller):
-    requirements = Requirements(35000, 20000)
     def __init__(self, attack=650, defense=5000) -> None:
         super().__init__(attack, defense)
-
 class Interceptor(Fighter, FighterKiller):
-    requirements = Requirements(1000, 200)
     def __init__(self, attack=180, defense=1000) -> None:
         super().__init__(attack, defense)
-
 class Frigate (Battleship, FighterKiller):
-    requirements = Requirements(10000, 10000)
     def __init__(self, attack=500, defense=2500) -> None:
         super().__init__(attack, defense)
