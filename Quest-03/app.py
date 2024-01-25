fb3bc6bfd0b887f957c69ec677fbd2c76135f897
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2024-01-25 18:17:48 +0100

    Quest-03

--- Quest-03/app.py
+++ Quest-03/app.py
@@ -1,15 +1,12 @@
+# from fastapi import FastAPI
 import os
 from dock_repositories import SpaceDockInMemoryRepository, SpaceDockFileRepository
 
+# app = FastAPI()
+
+
 def init_dock_repository():
     value = os.environ['DOCK_REPOSITORY'].lower()
-    # print(type(value))
-    # if value=='in_memory' :
-    #     print('1')
-    # elif value =='file':
-    #     print('2')
-    # else:
-    #     raise ValueError('Repository ' + value + ' does not exist')
     match value:
         case 'in_memory' :
             return SpaceDockInMemoryRepository()
